0ec35e886b54285939011676f9b7a295d0d40c74
C.Rosen <40198789+kelvban@users.noreply.github.com>
2021-05-07 16:41:23 +0800

    Update Swagger.java

--- demo/src/main/java/com/example/demo/util/config/Swagger.java
+++ demo/src/main/java/com/example/demo/util/config/Swagger.java
@@ -18,7 +18,7 @@ public class Swagger {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.honeywell.tms.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.xxx.xxx.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
